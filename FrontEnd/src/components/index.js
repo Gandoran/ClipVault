ef7a6129c1faffd1194fad0d966335f1ecd0899f
gandoran <giakicarloni@gmail.com>
2026-04-13 23:31:18 +0200

    refactor(components): riorganizzato la struttura delle componenti clip e aggiornati i file di importazione

--- FrontEnd/src/components/index.js
+++ FrontEnd/src/components/index.js
@@ -1,8 +1,8 @@
-export { ClipCard } from './ClipCard';
-export { ClipModal } from './ClipModal';
+export { ClipCard } from './clip/ClipCard';
+export { ClipModal } from './clip/ClipModal';
 export { FolderBar } from './FolderBar';
 export { AppHeader } from './AppHeader';
 export { DeleteButton } from './DeleteButton';
 export {SearchBar} from './SearchBar';
-export {ClipList} from './ClipList';
+export {ClipList} from './clip/ClipList';
 export {CommandPalette} from './CommandPalette';
